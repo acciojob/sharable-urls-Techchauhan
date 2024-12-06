e68b2f3f177a0d6e2c7dedaa60e7eff9da5a8a4f
Techchauhan <88832979+Techchauhan@users.noreply.github.com>
2024-12-06 12:58:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 // your code here
+document.getElementById("form1").addEventListener("submit",function(event){
+
+	event.preventDefault();
+
+	const name = document.getElementById("name").value;
+	const year = document.getElementById("year").value;
+
+	const url = `https://localhost:8080/?name=${encodeURIComponent(name)}&year=${encodeURIComponent(year)}`
+	
+	 document.getElementById("url").textContent = url;
+})
